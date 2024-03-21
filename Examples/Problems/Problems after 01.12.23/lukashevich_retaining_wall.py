3b6b3e7fde36047e3d777169524adeb729a49356
RusHollow_PNU <RusHollow@gmail.com>
2024-03-21 17:26:46 +1000

    work12 retaining wall

--- Examples/Problems/Problems after 01.12.23/lukashevich_retaining_wall.py
+++ Examples/Problems/Problems after 01.12.23/lukashevich_retaining_wall.py
@@ -27,15 +27,15 @@ mu_rw = 0.2  # mu for retaining wall
 trw = 1   # thickness
 gamma_rw = 24e3  # Н/m^3  own weight for retaining wall
 E_g_bot = 20000e6  # Spring stiffness 1!!!!
-qn = 263
-qt = 213
-qx1 = 68
-qx2 = 89
-qx3 = 142
-qx4 = 150
-qy = 45
-qgr1 = 471
-qgr2 = 462
+qn = 263e3
+qt = 213e3
+qx1 = 68e3
+qx2 = 89e3
+qx3 = 142e3
+qx4 = 150e3
+qy = 45e3
+qgr1 = 471e3
+qgr2 = 462e3
 h0, h1, h2, h3 = 5, 1, 1, 5.5
 L1, L2, L3 = 2, 2, 4.5
 L2_1 = 1.5
@@ -56,7 +56,6 @@ nodes.add_node(L1+L2+L3, h0+h1)  # 6
 nodes.add_node(L1+L2+L3, h0)  # 7
 nodes.add_node(L1, h0+h1+h2+h3)  # 8
 nodes.add_node(L1 + L2_1, h0+h1+h2+h3)  # 9
-nodes.add_node(0, h0-0.1)  # 10 SUP NODE
 
 # Set elements
 element_4node = Element4NodeLinearContainer(nodes_scheme=nodes)
@@ -78,9 +77,20 @@ element_macro.add_element(EN=[2, 8, 9, 4], frag_amount_h=int(h3/mesh_size), frag
 element_macro.fragment_all(element_4node, element_frame, element_null)
 # find all nodes alongside bottom of the retaining wall
 nodes_bot = nodes.find_nodes_numbers_along_segment(point1=(0, h0), point2=(L1+L2+L3, h0))
+# add bot nodes to support:
+print(nodes[-1].indices)
+nodes_sup = []
+k = len(nodes)
+for node in nodes_bot:
+    nodes.add_node(nodes[node].x, nodes[node].y-0.5)
+    nodes_sup.append(k)
+    k += 1
+    # print(nodes_sup[-1], node)
+    print(nodes[-1].indices)
 # n null elements and adding t null elements silently
-for i in nodes_bot:
-    element_null.add_element(EN=[0, i], cke=123, alpha=math.pi / 2, gap_length=0)
+for n_sup, n_bot in zip(nodes_sup, nodes_bot):
+    element_null.add_element(EN=[n_sup, n_bot], cke=123, alpha=math.pi / 2, gap_length=0)
+    print([n_sup, n_bot])
 
 side1 = nodes.find_nodes_numbers_along_segment((L1, h0+h1+h2+h3), (L1+L2_1, h0+h1+h2+h3), sorted_by_y=False)
 side2 = nodes.find_nodes_numbers_along_segment((L1+L2_1, h0+h1+h2+h3), (L1+L2, h0+h1+h2), sorted_by_y=False)
@@ -89,7 +99,8 @@ side4 = nodes.find_nodes_numbers_along_segment((L1+L2+L3, h0+h1), (L1+L2+L3, h0)
 
 # form R, RF and solve SLAE
 sm = StiffnessMatrix(nodes=nodes, el_frame=element_frame, el_4node=element_4node, el_null=element_null)
-sm.support_nodes([0], direction='hv')
+sm.support_nodes(nodes_sup, direction='h')
+sm.support_nodes(nodes_sup[0:3], direction='v')
 
 for spring_node_num in nodes_bot:
     sm.add_spring(degree_of_freedom=spring_node_num*2, stiffness=E_g_bot)
@@ -106,7 +117,7 @@ for i, nn in enumerate(side1):
         force /= 2
     lv.add_concentrated_force(force, degree_of_freedom=nn*2)
     lv.add_concentrated_force(-qt * length, degree_of_freedom=nn * 2 - 1)
-    print(f'vertical {force=}, {length=} {nn=}, dof={nn*2}')
+    print(f'vertical   {force=}, {length=} {nn=}, dof={nn*2}')
     print(f'horizontal force={-qt * length}, {length=} {nn=}, dof={nn * 2-1}')
 
 print('SIDE 2:')
@@ -116,8 +127,8 @@ for i, nn in enumerate(side2):
     force_h = (2 * (qx1 + qx2) / math.sqrt((L2-L2_1)*(L2-L2_1) + h3*h3) * length * i - qx1) * length
     lv.add_concentrated_force(-qy * length, degree_of_freedom=nn * 2)
     lv.add_concentrated_force(-force_h, degree_of_freedom=nn * 2 - 1)
-    print(f'vertical force={-qy * length}, {length=} {nn=}, dof={nn*2}')
-    print(f'horizontal force={force_h}, {length=} {nn=}, dof={nn * 2-1}')
+    print(f'vertical   force={-qy * length}, {length=} {nn=}, dof={nn*2}')
+    print(f'horizontal force={-force_h}, {length=} {nn=}, dof={nn * 2-1}')
 
 print('SIDE 3:')
 # SIDE 3 right top
@@ -125,33 +136,31 @@ for i, nn in enumerate(side3):
     length = math.sqrt((h2*h2) + (L3*L3)) / len(side3)
     lv.add_concentrated_force(-qgr1 * length, degree_of_freedom=nn * 2)
     lv.add_concentrated_force(-qx3 * length, degree_of_freedom=nn * 2 - 1)
-    print(f'vertical force={-qgr1 * length}, {length=} {nn=}, dof={nn*2}')
+    print(f'vertical   force={-qgr1 * length}, {length=} {nn=}, dof={nn*2}')
     print(f'horizontal force={-qx3 * length}, {length=} {nn=}, dof={nn * 2-1}')
-
+#
 print('SIDE 4:')
 # SIDE 4 right top
 for i, nn in enumerate(side4):
     length = h1 / len(side3)
-    lv.add_concentrated_force(-qgr2 * length, degree_of_freedom=nn * 2)
+    # lv.add_concentrated_force(-qgr2 * length, degree_of_freedom=nn * 2)
     lv.add_concentrated_force(-qx4 * length, degree_of_freedom=nn * 2 - 1)
-    print(f'vertical force={-qgr2 * length}, {length=} {nn=}, dof={nn*2}')
+    # print(f'vertical force={-qgr2 * length}, {length=} {nn=}, dof={nn*2}')
     print(f'horizontal force={-qx4 * length}, {length=} {nn=}, dof={nn * 2-1}')
 
 
 if not force_inc:
     pass
     # lv.add_own_weight_to_rf(nodes_scheme=nodes, element_container_list=[element_4node])
-
 else:
     pass
     # lv.add_own_weight_to_rf(nodes_scheme=nodes, element_container_list=[element_4node])
-
 
 # plot --------------------------------------------------------------------------
 # Calculation and plotting object
 graph = PlotScheme(nodes=nodes, sm=sm, lv_const=lv, lv_variable=lv_v,
                    element_frame=element_frame, element_container_obj=element_4node, element_null=element_null,
-                   partition=10, scale_def=200, autorun=autorun)
+                   partition=10, scale_def=2000, autorun=autorun)
 
 # calculate time
 end = time.time()
