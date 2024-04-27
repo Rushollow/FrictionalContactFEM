b9a9898f8c2c8b890565222aaa5e1aa8e18a377d
RusHollow_PNU <RusHollow@gmail.com>
2024-04-27 18:40:00 +1000

    work18 modified article8 file

--- input_data.py
+++ input_data.py
@@ -19,7 +19,7 @@ SCHEME_THICKNESS = 1
 SCALE_DEF = 50
 
 # friction coefficient
-FRICTION_COEFFICIENT = 1
+FRICTION_COEFFICIENT = 0.9
 # limit of steps for lcp_solve Lemke
 LEMKE_LIMIT_STEPS = 800
 
