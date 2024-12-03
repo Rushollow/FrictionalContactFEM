6f8280f15d152ee1a0b52f957f2fceaffa755299
RusHollow_PNU <RusHollow@gmail.com>
2024-12-03 16:32:12 +1000

    work20 modified ansys_lira_abaqus_compare.py

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ import socket
 import math
 import time
 import numpy as np
-
+import pyqtgraph as pg
 from scipy.optimize import minimize
 
 from FEM.scheme import NodeContainer, StiffnessMatrix, LoadVector
@@ -18,121 +18,115 @@ from input_data import FRICTION_COEFFICIENT
 assert FRICTION_COEFFICIENT == 0.5, 'Friction coef need to be 0.5'
 
 
-start = time.time()
-np.set_printoptions(formatter={'float': lambda x: "{0:0.5f}".format(x)})
+start_time = time.time()
 
-# add nodes # for 4 node element
-nodes = NodeContainer()
-nodes.add_node(0, 0)
-nodes.add_node(1.75, 0)
-nodes.add_node(1.75, 1.75)
-nodes.add_node(0, 1.75)
-# for frame
-nodes.add_node(0, 1.75)
-nodes.add_node(1.75, 1.75)
-nodes.add_node(2.5, 2.75)
-nodes.add_node(4, 2.75)
-# for 2nd 4node element
-nodes.add_node(2.5, 2.75)
-nodes.add_node(4, 2.75)
-nodes.add_node(4, 4)
-nodes.add_node(2.5, 4)
-XY1, XY2 = [], []
-i = 0
-for node in nodes:
-    XY1.append(node.x)
-    XY2.append(node.y)
-    i += 1
+start = 0
+stop = 100
+char_limit = 3
 
-# set inputs
-Ar = 0.2
-Er = 2e11
-Ix = 2.3 * 10**(-4)
-E = 5e7
-F = 1e6
-gamma = 5
-t = 1
-mu = 0.3
+y = []
+def true_len(i, j, k):
+    s = ''
+    if i != 0: s += str(i)
+    if j != 0: s += str(j)
+    if k != 0: s += str(k)
+    return len(s)
 
-# add elements
-element_4node = Element4NodeLinearContainer(nodes_scheme=nodes)
-element_4node.add_element(EN=[0, 1, 2, 3], E=E, mu=mu, t=t)
-element_4node.add_element(EN=[8, 9, 10, 11], E=E, mu=mu, t=t)
+for i in range(start, stop):
+    if i == 1: continue
+    for j in range(start, stop):
+        if j ==1: continue
+        for k in range(start, stop):
+            if k == 1: continue
+            val = i*i + j*j + k*k
+            if val not in y and true_len(i, j, k) < char_limit:
+                y.append(val)
+y = sorted(y)
+x = list(range(len(y)))
 
-# add frame elements
-element_frame = ElementFrameContainer(nodes_scheme=nodes)
-element_frame.add_element(EN=[4, 5], E=Er, A=Ar, I=Ix)
-element_frame.add_element(EN=[5, 6], E=Er, A=Ar, I=Ix)
-element_frame.add_element(EN=[6, 7], E=Er, A=Ar, I=Ix)
-# n null elements and adding t null elements silently
-element_null = ElementNullContainer(nodes_scheme=nodes)
-element_null.add_element(EN=[3, 4], cke=123, alpha=math.pi / 2)
-element_null.add_element(EN=[2, 5], cke=123, alpha=math.pi / 2)
-element_null.add_element(EN=[6, 8], cke=123, alpha=math.pi / 2)
-element_null.add_element(EN=[7, 9], cke=123, alpha=math.pi / 2)
-
-# form R, RF and solve SLAE
-sm = StiffnessMatrix(nodes=nodes, el_frame=element_frame, el_4node=element_4node, el_null=element_null)
-nodes_to_support = [0, 1, 10, 11]
-sm.support_nodes(nodes_to_support, direction='hv')
-lv = LoadVector()
-lv.add_concentrated_force(force=F, degree_of_freedom=14)
-lv.add_concentrated_force(force=-F, degree_of_freedom=8)
-lv.add_concentrated_force(force=F * 1.4, degree_of_freedom=13)
-lv.add_concentrated_force(force=-F * 2, degree_of_freedom=11)
-
-
-# plot --------------------------------------------------------------------------
-# Calculation and plotting object
-graph = PlotScheme(nodes=nodes, sm=sm, lv_const=lv, lv_variable=None,
-                   element_frame=element_frame, element_container_obj=element_4node, element_null=element_null,
-                   partition=10, scale_def=25, autorun=True)
-
-# calculate time
-end = time.time()
-last = end - start
-print("Time Lemke: ", last)
+# plot = pg.plot()
+# line = plot.plot(arr[0], arr[1], pen='g', symbol='x', symbolPen='g',
+#                   symbolBrush=0.2, name='green')
 
 
 
-# if __name__ == "__main__":
-#     graph.fill_arrays_scheme()  # form info for plot at UI
-#     application(graph)
+
+print(f'TIME: {time.time()-start_time}')
+
+# importing Qt widgets
+from PyQt5.QtWidgets import *
+import sys
+
+# importing pyqtgraph as pg
+import pyqtgraph as pg
+from PyQt5.QtGui import *
 
 
-start = time.time()
+# Bar Graph class
+class BarGraphItem(pg.BarGraphItem):
 
-rows = int((graph.intl_table.table.shape[0]))
-M = graph.intl_table.table[:, rows:rows*2]
-q = np.array(graph.intl_table.table[:, -1])
-mu = 10
+    # constructor which inherit original
+    # BarGraphItem
+    def __init__(self, *args, **kwargs):
+        pg.BarGraphItem.__init__(self, *args, **kwargs)
 
-# Функция штрафа
-def penalty_function(x0):
-    z = x0[:rows]
-    x = x0[rows:]
-    penalty = 0.5 * np.linalg.norm(x - M @ z - q)**2
-    penalty += (mu / 2) * np.dot(x, z)
-    return penalty
-
-# Ограничения: z >= 0, w >= 0
-bounds = []
-for i in range(rows * 2):
-    bounds.append((0, None))
-
-# Начальное приближение
-x0 = np.zeros(rows * 2)
-
-# Решение задачи оптимизации
-result = minimize(penalty_function, x0, bounds=bounds)
-
-# Получение результатов
-z_opt = result.x[:rows]
-x_opt = result.x[rows:]
-print(f'Penalty solved in:{time.time() - start}. sec')
+    # creating a mouse double click event
+    def mouseDoubleClickEvent(self, e):
+        # setting scale
+        self.setScale(0.2)
 
 
-print(f'zn:{graph.lemke.zn_anim[-1]} xn:{graph.lemke.xn_anim[-1]}')
-print(f'zt:{graph.lemke.zt_anim[-1]} xt:{graph.lemke.xt_anim[-1]}')
-print(f'zn:{z_opt} xn:{x_opt}')
+class Window(QMainWindow):
 
+    def __init__(self):
+        super().__init__()
+
+        # setting title
+        self.setWindowTitle("PyQtGraph")
+
+        # setting geometry
+        self.setGeometry(100, 100, 600, 500)
+
+        # calling method
+        self.UiComponents()
+
+        # showing all the widgets
+        self.show()
+
+    # method for components
+    def UiComponents(self):
+        # creating a widget object
+        widget = QWidget()
+
+        # creating a new label
+        label = QLabel("Line Plot")
+
+
+        # create plot window object
+        plt = pg.plot()
+
+        # adding legend
+        plt.addLegend()
+
+        # set properties of the label for y axis
+        plt.setLabel('left', 'number', units='i*i + j*j + k*k')
+
+        # set properties of the label for x axis
+        plt.setLabel('bottom', 'order', units='None')
+
+        plt.plot(x, y, pen='b', symbol='x',
+                    symbolPen='g', symbolBrush=0.3, name='blue')
+
+
+        # setting this widget as central widget of the main window
+        self.setCentralWidget(widget)
+
+
+# create pyqt5 app
+App = QApplication(sys.argv)
+
+# create the instance of our Window
+window = Window()
+
+# start the app
+sys.exit(App.exec())
